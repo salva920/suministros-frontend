a85bededc899c85c87a8bc933880eabefbbf2472
Carmenuvm <cmcn5toe2017@gmail.com>
2025-04-28 19:44:42 -0400

    agg listado de precio

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -128,7 +128,7 @@ const Login = () => {
 
     setLoading(true);
     try {
-      const response = await axios.post(`${API_URL}/login`, {
+      const response = await axios.post(`${API_URL}/auth/login`, {
         username,
         password 
       });
